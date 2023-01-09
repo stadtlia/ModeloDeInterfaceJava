25b534b9af744c0febc5093f9b58e8bbc3634d5b
stadtlia <lidiastadtlober@outlook.com>
2023-01-09 19:06:03 -0300

    first commit

---
+++ src/main/java/com/mycompany/interfacegraficajava/InterfaceGraficaJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.interfacegraficajava;
+
+/**
+ *
+ * @author renan
+ */
+public class InterfaceGraficaJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
